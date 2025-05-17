2e0dc99f77eba320b1497acb0f8ffefa8781b4af
Agatha Paiva <agathahost@gmail.com>
2025-05-16 23:28:48 -0300

    test unit - class controller method post and getAll

---
+++ src/test/java/com/br/fiap/fortaleza/sabor/application/usecase/CreateUseCaseTest.java
@@ -0,0 +1,12 @@
+package com.br.fiap.fortaleza.sabor.application.usecase;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateUseCaseTest {
+
+    @Test
+    void save() {
+    }
+}
