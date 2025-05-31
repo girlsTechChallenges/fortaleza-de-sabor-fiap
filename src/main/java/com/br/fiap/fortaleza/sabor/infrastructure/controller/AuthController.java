860d4fb34bbc022be5428d4a84fd0b05b07cd7fe
agathapaiiva <agathahost@gmail.com>
2025-05-31 14:33:20 -0300

    ajustes (#27)

--- src/main/java/com/br/fiap/fortaleza/sabor/infrastructure/controller/AuthController.java
+++ src/main/java/com/br/fiap/fortaleza/sabor/infrastructure/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController()
+@RestController
 @RequestMapping("/auth")
 public class AuthController {
 
