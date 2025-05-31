00f1afe6d82c6214dc02e108434a5eb9b9e8b859
Agatha Paiva <agathahost@gmail.com>
2025-05-31 14:03:11 -0300

    implementação de teste unitarios - controller autenticação e use case

--- src/test/java/com/br/fiap/fortaleza/sabor/mock/MockUser.java
+++ src/test/java/com/br/fiap/fortaleza/sabor/mock/MockUser.java
@@ -4,10 +4,12 @@ import com.br.fiap.fortaleza.sabor.domain.address.Address;
 import com.br.fiap.fortaleza.sabor.domain.enums.TypeEnum;
 import com.br.fiap.fortaleza.sabor.domain.user.User;
 import com.br.fiap.fortaleza.sabor.infrastructure.controller.dto.AddressDto;
+import com.br.fiap.fortaleza.sabor.infrastructure.controller.dto.UserAuthDto;
 import com.br.fiap.fortaleza.sabor.infrastructure.controller.dto.UserResponseDto;
 import com.br.fiap.fortaleza.sabor.infrastructure.persistence.AddressEntity;
 import com.br.fiap.fortaleza.sabor.infrastructure.persistence.UserEntity;
 import com.br.fiap.fortaleza.sabor.infrastructure.persistence.enums.TypeEntityEnum;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -40,9 +42,9 @@ public class MockUser {
     public static User userMockOne(){
         return new User(
                 "João Silva",
-                "email",
+                "email@email.com.br",
                 "login",
-                "senha",
+                new BCryptPasswordEncoder().encode("senha1234"),
                 LocalDate.of(2025,5,16),
                 TypeEnum.DONO,
                 List.of(new Address("rua", "bairro", "complemento",
@@ -80,4 +82,9 @@ public class MockUser {
                 List.of(new AddressDto("rua", "bairro", "complemento",
                         0, "estado", "cidade","03565000")));
     }
+
+    public static UserAuthDto userAuthDto(){
+        return new UserAuthDto("RyQrkn3JBIhPESrJcH4AT036ubAmlosR3HnwHT3NmR2GwCXEiSVo0FLW3z4MHJrbel4zKvFc_EySsAFr5xlu9kSFN5LKcvXcehawI4eN5jyRpSGrxe1MrcZnFsA4jDUT9Dmd4BqXtMw7hiP7eOKBxDeovxRoOMSI_4rHF_BAQQvahQ", 5L);
+    }
+
 }
